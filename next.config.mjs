322009250242900303e6ca2e56161aa711878325
mayurgaikwad7474 <mayurgaikwad7474@gmail.com>
2024-01-26 17:48:56 +0530

    build file updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export",
-    skipTrailingSlashRedirect: true,
-    trailingSlash: true,
     images: {
         domains: ['i.ibb.co']
     }
