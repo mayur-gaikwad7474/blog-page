ec55a4cd23973d1af6bf789352f4023e316adb5e
mayurgaikwad7474 <mayurgaikwad7474@gmail.com>
2024-01-26 17:42:40 +0530

    updated the changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: "export",
     images: {
         domains: ['i.ibb.co']
-    }
+    },
+    exportPathMap: async function () {
+        return {
+            '/': { page: '/' },
+        };
+    },
 };
 
 export default nextConfig;
